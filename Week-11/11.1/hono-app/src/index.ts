f73891b05243db36748c42ad139614bbc60edf1a
Eng99Arjun <dhananjay.99jb@gmail.com>
2025-07-03 12:54:50 +0530

    feat: add authorization middleware and update TypeScript configuration

--- Week-11/11.1/hono-app/src/index.ts
+++ Week-11/11.1/hono-app/src/index.ts
@@ -2,7 +2,22 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.get('/', (c) => {
+async function authMiddleware(c:any, next: any){
+  if(c.req.header("Authorization")){
+    await next()
+  } else {
+    return c.text("You don't have access");
+  }
+}
+
+app.get('/', authMiddleware ,async (c) => {
+  //body, headers, query, middlewares, connecting to a database
+  const body = await c.req.json()
+  console.log(body);
+  console.log(c.req.header("Authorization"));
+  console.log(c.req.query("param"));
+  console.log('Hello Hono!')
+
   return c.text('Hello Hono!')
 })
 
